59bb2f9e333de0e5144dd7767ac10e6b3df19964
mwitek <matt@mwitekdesign.com>
2012-12-14 02:30:15 -0500

    commenting out retina

--- javascripts/retina.js
+++ javascripts/retina.js
@@ -1,11 +1,14 @@
-var retina = window.devicePixelRatio > 1; //if ration is greater than 1 its retina
-if (retina) { // if this is true...
-  $('img').each(function() { // get all images and run this code on 'each' image found
-  var src = $(this).attr("src"); // save the source on the image
-
-  $(this).attr("src", src.replace(/\./i, "@2x."));
-  $("img").error(function () {
-    $(this).unbind("error").attr("src", src);
-  }); 
-});
-}
+// var retina = window.devicePixelRatio > 1; //if ration is greater than 1 its retina
+// if (retina) { // if this is true...
+//   $('img').each(function() { // get all images and run this code on 'each' image found
+//   var src = $(this).attr("src"); // save the source on the image
+//   var orgWidth = $(this).css('width');
+//   var orgHeight = $(this).css('height');
+//   console.log("width: " + orgWidth + " height: " + orgHeight)
+//   $(this).attr("src", src.replace(/\./i, "@2x.")).css({width:width,});
+//   $(this).css({width:width,height:height});
+//   $("img").error(function () {
+//     $(this).unbind("error").attr("src", src);
+//   }); 
+// });
+// }
