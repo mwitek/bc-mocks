a61cac511f7f48c1f250b20758244a3a9362fe44
Matthew Witek <Mwitek@matthew-witeks-imac.local>
2012-12-13 21:34:02 -0500

    added image replace back in

--- javascripts/logo.rotator.js
+++ javascripts/logo.rotator.js
@@ -37,7 +37,11 @@ function LogoRotatorInit(){
     $logo = $('.'+brandLogoSelector[random]),
     originalOpacity = $logo.css('opacity');
     
-
+    $logo.animate({opacity: 0.0}, 2000, function() {
+      var randomImage = brandLogoImages[Math.floor(Math.random() * brandLogoImages.length)];
+      $(this).children('img').attr('src',randomImage);
+      $(this).animate({opacity:originalOpacity},2000)
+    });
   }
 
   window.setInterval(function(){
